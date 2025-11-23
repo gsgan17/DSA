c8d26d4355682cdb6a98cbe36b49940d488fb09a
gsgan17 <gagandeepsingh.gs1700@gmail.com>
2025-11-23 11:59:54 +0530

    Time: 50 ms (12.72%) | Memory: 42.2 MB (31.64%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -9,7 +9,6 @@ public:
             for(int j=0; j<3; j++){
                 dp[i][j]=max(dp[i][j], dp[i-1][j]);
                 int sum = dp[i-1][j]+nums[i-1];
-                // cout << "sum at " << i << " " << j << " " << sum << endl;
                 int mod = sum%3;
                 dp[i][mod]=max(dp[i][mod], sum);
             }
