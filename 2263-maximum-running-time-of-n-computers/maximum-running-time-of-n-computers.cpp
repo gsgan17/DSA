2f878e69edb8530af14574277846309a7ddcb0c4
gsgan17 <gagandeepsingh.gs1700@gmail.com>
2025-12-01 16:49:56 +0530

    Time: 47 ms (13.06%) | Memory: 59.7 MB (58.92%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPossible(long long time, vector<int>& batteries, int n){
+        int nb = batteries.size();
+        long long tempTime = time*n;
+        for(int i=0; i<nb; i++){
+            tempTime-= min(time, (long long)(batteries[i]));
+        }
+        return tempTime<=0;
+    }
+    long long maxRunTime(int n, vector<int>& batteries) {
+        int nb = batteries.size();
+        long long total = accumulate(batteries.begin(), batteries.end(), 0ll);
+        sort(batteries.begin(), batteries.end(), greater<int>());
+
+        long long soln = -1;
+        long long l = 0;
+        long long r = total;
+
+        while(l<=r){
+            long long mid = l + (r-l)/2;
+            if(isPossible(mid, batteries, n)){
+                soln = mid;
+                l = mid + 1;
+            } else{
+                r = mid - 1;
+            }
+        }
+
+        return soln;
+    }
+};
