388656f52c810c1c78b04b4c0598319aeea7f3cb
gsgan17 <gagandeepsingh.gs1700@gmail.com>
2025-12-02 08:07:05 +0530

    Time: 55 ms (93.68%) | Memory: 191.5 MB (88.68%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -1,29 +1,48 @@
+// class Solution {
+// public:
+//     long long c(long long n){
+//         return n*(n-1)/2;
+//     }
+//     int countTrapezoids(vector<vector<int>>& points) {
+//         int mod = 1e9 + 7;
+//         int n = points.size();
+//         map<int, int> levelPoints;
+
+//         for(int i=0; i<n; i++){
+//             levelPoints[points[i][1]]++;
+//         }
+
+//         long long soln = 0;
+//         long long edges = 0;
+
+//         for(auto it: levelPoints){
+//             long long numEdge = c(it.second);
+//             soln += numEdge*edges;
+//             edges+=numEdge;
+//             soln%=mod;
+//         }
+//         soln%=mod;
+
+//         return soln;
+
+//     }
+// };
+
+// Optimized
 class Solution {
 public:
-    long long c(long long n){
-        return n*(n-1)/2;
-    }
     int countTrapezoids(vector<vector<int>>& points) {
-        int mod = 1e9 + 7;
-        int n = points.size();
-        map<int, int> levelPoints;
-
-        for(int i=0; i<n; i++){
-            levelPoints[points[i][1]]++;
+        unordered_map<int, int> pointNum;
+        const int mod = 1e9 + 7;
+        long long ans = 0, sum = 0;
+        for (auto& point : points) {
+            pointNum[point[1]]++;
         }
-
-        long long soln = 0;
-        long long edges = 0;
-
-        for(auto it: levelPoints){
-            long long numEdge = c(it.second);
-            soln += numEdge*edges;
-            edges+=numEdge;
-            soln%=mod;
+        for (auto& [_, pNum] : pointNum) {
+            long long edge = (long long)pNum * (pNum - 1) / 2;
+            ans += edge * sum;
+            sum += edge;
         }
-        soln%=mod;
-
-        return soln;
-
+        return ans % mod;
     }
 };
