07424f979840376ad963182768b7bd0842111b54
Gagandeep Singh <gs.gagandeepsingh17@gmail.com>
2026-08-04 19:48:55 +0530

    Sync Codeforces: 2218A - The 67th Integer Problem [C++23 (GCC 14-64, msys2)]

---
+++ codeforces-solutions/2218A_The_67th_Integer_Problem.cpp
@@ -0,0 +1,43 @@
+// goshawk17
+#include <bits/stdc++.h>
+using namespace std;
+ 
+class Solution {
+public:
+	void solveUtil() {
+		int x;
+		cin >> x;
+		int y = x;
+		cout << y << "\n";
+	}
+ 
+	void solve() {
+		long long t;
+		cin >> t;
+ 
+		while (t--) {
+			solveUtil();
+		}
+ 
+		// solveUtil();
+	}
+};
+ 
+int main() {
+ 
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+ 
+#ifndef ONLINE_JUDGE
+	// for getting input from input file.
+	freopen("input.txt", "r", stdin);
+	// for writing output to output file.
+	freopen("output.txt", "w", stdout);
+#endif
+ 
+	Solution soln;
+	soln.solve();
+ 
+	return 0;
+ 
+}
