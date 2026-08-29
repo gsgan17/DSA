34745eee6ad9926b2b5b4f99f1b54a3f03b900f9
Gagandeep Singh <gs.gagandeepsingh17@gmail.com>
2026-08-29 17:07:24 +0530

    Time: 7 ms (22.69%) | Memory: 12 MB (66.67%) - LeetSync

---
+++ leetcode-solutions/133-clone-graph/clone-graph.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+private:
+    unordered_map<Node*, Node*> mp;
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr) {
+            return node;
+        }
+        if (mp.count(node)) {
+            return mp[node];
+        }
+
+        Node* copy = new Node(node->val);
+        mp[node] = copy;
+        for (auto it : node->neighbors) {
+            copy->neighbors.push_back(cloneGraph(it));
+        }
+
+        return copy;
+    }
+};
